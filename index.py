a5795a9546b39b2ce0171a9b8bab8f1b557cf126
tkayops <bruceomondi960@gmail.com>
2025-07-26 23:55:27 +0300

    rearragement

--- index.py
+++ index.py
@@ -4,11 +4,17 @@ nest_asyncio.apply()
 
 import os
 
-os.environ["MISTRAL_API_KEY"] = "0uLqHjUaxWHIBObcCbwSac4J3l0CLuas"
+
 
 from llama_index.llms.mistralai import MistralAI
 from llama_index.embeddings.mistralai import MistralAIEmbedding
 from llama_index.core import Settings
+from llama_index.core import SimpleDirectoryReader
+from llama_index.core import VectorStoreIndex
+
+
+
+os.environ["MISTRAL_API_KEY"] = "0uLqHjUaxWHIBObcCbwSac4J3l0CLuas"
 
 llm = MistralAI(model="open-mixtral-8x22b", temperature=0.1)
 embed_model = MistralAIEmbedding(model_name="mistral-embed")
@@ -16,15 +22,14 @@ embed_model = MistralAIEmbedding(model_name="mistral-embed")
 Settings.llm = llm
 Settings.embed_model = embed_model
 
-from llama_index.core import SimpleDirectoryReader
-
+#Loading Data
 uber_docs = SimpleDirectoryReader(input_files=["./documents/Constitution.pdf"]).load_data()
 
-from llama_index.core import VectorStoreIndex
-
+#Indexing Data
 uber_index = VectorStoreIndex.from_documents(uber_docs)
 uber_query_engine = uber_index.as_query_engine(similarity_top_k=5)
 
+#Querying Data
 response = uber_query_engine.query("When could a passport be denied ? give with detailed explanation giveall the criteria")
 print(response)
 
